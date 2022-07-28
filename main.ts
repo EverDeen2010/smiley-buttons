8d2ff9fa0c35f05d69ad3134e6ecda47509b584c
EverDeen2010 <110064243+EverDeen2010@users.noreply.github.com>
2022-07-28 14:25:07 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -43,6 +43,48 @@ input.onButtonPressed(Button.AB, function () {
         # . . . #
         . # # # .
         `)
+    basic.showLeds(`
+        # # . # #
+        # # . # #
+        . . . . .
+        # . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # . # #
+        # # . # #
+        . . . . .
+        # . . . .
+        . # . . .
+        `)
+    basic.showLeds(`
+        # # . # #
+        # # . # #
+        . . . . .
+        # . . . .
+        . # # . .
+        `)
+    basic.showLeds(`
+        # # . # #
+        # # . # #
+        . . . . .
+        # . . . .
+        . # # # .
+        `)
+    basic.showLeds(`
+        # # . # #
+        # # . # #
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # . .
+        . . . . .
+        . . # . .
+        `)
 })
 input.onButtonPressed(Button.B, function () {
     basic.showLeds(`
